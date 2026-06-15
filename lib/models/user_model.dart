45ef9b90537ad965ed8c84b5a7005c4874cf044e
yutari <w1ldcvrd@mail.ru>
2026-06-15 11:55:54 +0500

    Add UserModel

---
+++ lib/models/user_model.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  final int id;
+  final String name;
+  final String targetLevel;
+  final String createdAt;
+
+  UserModel({
+    required this.id,
+    required this.name,
+    required this.targetLevel,
+    required this.createdAt,
+  });
+}
