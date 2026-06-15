2788c3a05520a2698b436cf2d7e40e1e360b3d3d
yutari <w1ldcvrd@mail.ru>
2026-06-15 12:57:02 +0500

    Add profile and settings routes to navigation

--- lib/navigation/routes.dart
+++ lib/navigation/routes.dart
@@ -1,7 +1,9 @@
 // Именованные маршруты приложения
 const String kRouteHome = '/';
-const String kRouteJlptLevels = '/jlpt-levels'; // список JLPT-уровней
-const String kRouteKanjiList = '/kanji-list';   // список канджи выбранного уровня
+const String kRouteJlptLevels = '/jlpt-levels';
+const String kRouteKanjiList = '/kanji-list';
 const String kRouteKanjiDetail = '/kanji-detail';
 const String kRouteQuiz = '/quiz';
 const String kRouteProgress = '/progress';
+const String kRouteProfile = '/profile';
+const String kRouteSettings = '/settings';
