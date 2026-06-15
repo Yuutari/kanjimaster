e0e174a9daa25b18e492b59a8fdcee1b28897ea6
yutari <w1ldcvrd@mail.ru>
2026-06-15 12:07:25 +0500

    Add routes.dart
    Define named routes for the application.

---
+++ lib/navigation/routes.dart
@@ -0,0 +1,7 @@
+// Именованные маршруты приложения
+const String kRouteHome = '/';
+const String kRouteJlptLevels = '/jlpt-levels'; // список JLPT-уровней
+const String kRouteKanjiList = '/kanji-list';   // список канджи выбранного уровня
+const String kRouteKanjiDetail = '/kanji-detail';
+const String kRouteQuiz = '/quiz';
+const String kRouteProgress = '/progress';
