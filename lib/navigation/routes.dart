7339da80dfe4cf5a1d9652955585f5a5c626d3e6
yutari <w1ldcvrd@mail.ru>
2026-06-15 14:20:18 +0500

    Remove unused navigation files

--- lib/navigation/routes.dart
+++
@@ -1,9 +0,0 @@
-// Именованные маршруты приложения
-const String kRouteHome = '/';
-const String kRouteJlptLevels = '/jlpt-levels';
-const String kRouteKanjiList = '/kanji-list';
-const String kRouteKanjiDetail = '/kanji-detail';
-const String kRouteQuiz = '/quiz';
-const String kRouteProgress = '/progress';
-const String kRouteProfile = '/profile';
-const String kRouteSettings = '/settings';
