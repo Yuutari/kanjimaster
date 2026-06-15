4e88cc842a9dbc4506f4c4e9b314ea5cf8054185
yutari <w1ldcvrd@mail.ru>
2026-06-15 11:55:24 +0500

    Add ExampleModel

---
+++ lib/models/example_model.dart
@@ -0,0 +1,15 @@
+class ExampleModel {
+  final int id;
+  final int kanjiId;
+  final String word;
+  final String reading;
+  final String translation;
+
+  ExampleModel({
+    required this.id,
+    required this.kanjiId,
+    required this.word,
+    required this.reading,
+    required this.translation,
+  });
+}
